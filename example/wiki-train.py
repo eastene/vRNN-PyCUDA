a39eda0fc2672b5635295e3c376846ea35342a85
evan <eastene@gmail.com>
2018-04-12 14:27:35 -0600

    create skeleton for RNN, update example

--- example/wiki-train.py
+++ example/wiki-train.py
@@ -1,6 +1,38 @@
+import argparse
 import json
+import os
+
 
 # parse single json object at a time
 def parse_file(file):
     for line in file:
         yield json.loads(line)
+
+
+def train():
+    pass
+
+
+def run():
+    pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Train or run RNN using example Wikipedia data.")
+    parser.add_argument('--train', action='store_true', dest='train',
+                        help='train on example datasets')
+    parser.add_argument('--run', action='store_false', dest='train',
+                        help='generate article text, requires trained model')
+    parser.add_argument('--profile', dest='profile')
+    parser.set_defaults(train=True)
+
+    args = parser.parse_args()
+
+    if args.train:
+        train()
+    else:
+        run()
+
+
+if __name__ == "__main__":
+    main()
---
+++ model/RNN.py
@@ -0,0 +1,10 @@
+class RNN:
+
+    def __init__(self):
+        pass
+
+    def rnn_forward(self):
+        pass
+
+    def rnn_backward(self):
+        pass
